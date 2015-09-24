c88b9d7cb43c8afb2bc7634132038fc4312a1076
Marcus Oskarsson <marcus.oscarsson@esrf.fr>
2015-09-24 16:34:20 +0200

    Reverted changes

--- src/app/bootstrap.php
+++ src/app/bootstrap.php
@@ -47,7 +47,7 @@ require_once 'functions.php';
 /* Config Loading or Installation */
 try
 {
-  $config = new USVN_Config_Ini(USVN_CONFIG_FILE, USVN_CONFIG_SECTION, array('create' => true)));
+  $config = new USVN_Config_Ini(USVN_CONFIG_FILE, USVN_CONFIG_SECTION);
   if (empty($config) && file_exists(USVN_CONFIG_FILE))
     {
       echo 'Config file unreadeable';
